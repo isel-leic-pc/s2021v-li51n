5111d0d03c961e213f15e0273b6243ed0095e173
Pedro Felix <pmhsfelix@gmail.com>
2021-04-06 20:41:12 +0100

    Examples and diagrams for the 2021-04-06 class

--- jvm/examples/src/main/java/pt/isel/pc/examples/synchronizers/NAryFifoSemaphoreWithKernelStyleAndSpecificNotification.java
+++ jvm/examples/src/main/java/pt/isel/pc/examples/synchronizers/NAryFifoSemaphoreWithKernelStyleAndSpecificNotification.java
@@ -3,28 +3,37 @@ package pt.isel.pc.examples.synchronizers;
 import pt.isel.pc.utils.NodeLinkedList;
 import pt.isel.pc.utils.Timeouts;
 
-public class NAryFifoSemaphoreWithKernelStyle {
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-    private final Object monitor = new Object();
+public class NAryFifoSemaphoreWithKernelStyleAndSpecificNotification {
+
+    private final Lock monitor = new ReentrantLock();
 
     private static class Request {
         final int requestedUnits;
+        final Condition condition;
         boolean isDone = false;
 
-        Request(int requestedUnits) {
+        Request(int requestedUnits, Condition condition) {
+
             this.requestedUnits = requestedUnits;
+            this.condition = condition;
         }
     }
 
     private int units;
     private final NodeLinkedList<Request> requests = new NodeLinkedList<>();
 
-    public NAryFifoSemaphoreWithKernelStyle(int initialUnits) {
+    public NAryFifoSemaphoreWithKernelStyleAndSpecificNotification(int initialUnits) {
         units = initialUnits;
     }
 
     public boolean acquire(int unitsToAcquire, long timeout) throws InterruptedException {
-        synchronized (monitor) {
+        monitor.lock();
+        try {
             // 1. fast-path
             if (requests.isEmpty() && units >= unitsToAcquire) {
                 units -= unitsToAcquire;
@@ -39,14 +48,17 @@ public class NAryFifoSemaphoreWithKernelStyle {
             // - compute wait deadline and current remaining
             long deadline = Timeouts.deadlineFor(timeout);
             long remaining = Timeouts.remainingUntil(deadline);
-            NodeLinkedList.Node<Request> myRequest = requests.enqueue(new Request(unitsToAcquire));
+            Condition myCondition = monitor.newCondition();
+            NodeLinkedList.Node<Request> myRequest = requests.enqueue(new Request(
+                    unitsToAcquire,
+                    myCondition));
             while (true) {
                 // 4. wait
                 try {
-                    monitor.wait(remaining);
+                    myCondition.await(remaining, TimeUnit.MILLISECONDS);
                 } catch (InterruptedException e) {
                     // Is the request done?
-                    if(myRequest.value.isDone) {
+                    if (myRequest.value.isDone) {
                         // If so, it needs to return with success
                         Thread.currentThread().interrupt();
                         return true;
@@ -72,26 +84,27 @@ public class NAryFifoSemaphoreWithKernelStyle {
                     return false;
                 }
             }
+        } finally {
+            monitor.unlock();
         }
     }
 
     public void release(int unitsToRelease) {
-        synchronized (monitor) {
+        monitor.lock();
+        try {
             units += unitsToRelease;
             completeAllPossible();
+        } finally {
+            monitor.unlock();
         }
     }
 
     private void completeAllPossible() {
-        if (requests.isNotEmpty() && units >= requests.getHeadValue().requestedUnits) {
-            do {
-                NodeLinkedList.Node<Request> headNode = requests.pull();
-                headNode.value.isDone = true;
-                units -= headNode.value.requestedUnits;
-            } while (requests.isNotEmpty() && units >= requests.getHeadValue().requestedUnits);
-            monitor.notifyAll();
+        while (requests.isNotEmpty() && units >= requests.getHeadValue().requestedUnits) {
+            NodeLinkedList.Node<Request> headNode = requests.pull();
+            headNode.value.isDone = true;
+            headNode.value.condition.signal();
+            units -= headNode.value.requestedUnits;
         }
-
     }
-
 }
